673731e37b9b3bd19236574fb933a086010f0aee
Daniel Huang & Si Cheng Zhou <pair+danielkyh.szow79@devbootcamp.com>
2015-10-30 19:43:42 -0700

    able to pass objects from express to hogen

--- Park.ly/routes/index.js
+++ Park.ly/routes/index.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var model = require('../models')
+var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  model.Spot.findAll().then(function(spot){
-  var user = spot[3]
-  var spot1 = Spot.create(price: 4, address: "")
-  user.setSpot([])
 
-    res.render('index', { title: 'Hello', spot: spot[3].dataValues })
-  })
+  models.User.findAll({
+    include: [ models.Spot ]
+  }).then(function(users) {
+    // console.log(users)
+    // console.log(users[0].Spots)
+    res.render('index', {
+      title: 'Express',
+      users: users
+    });
+  });
+
+    // res.render('index', { title: 'Hello' })
+
   // console.log(data)
   // model.User.create
-
 });
 
 module.exports = router;

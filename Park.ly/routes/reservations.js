7c3d3a34ec429e06d4161599bd234d873b800024
Daniel Huang & Si Cheng Zhou <pair+danielkyh.szow79@devbootcamp.com>
2015-10-31 16:06:26 -0700

    add two user routes @ express

---
+++ Park.ly/routes/reservations.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+// var models = require('../models')
+
+router.get('/', function(req, res, next) {
+  // console.log(req.params.id)
+
+  res.send('you are in reservations');
+});
+
+module.exports = router;
--- Park.ly/routes/users.js
+++ Park.ly/routes/users.js
@@ -1,32 +1,45 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var models = require('../models');
 
 // User creation route
-router.post('/new', function(req, res, next) {
-  res.send('you are in route to create user');
+router.post('/', function(req, res) {
+  models.User.create({first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, password: req.body.password})
+  res.redirect('../');
 });
 
 // User profile route
 router.get('/:id', function(req, res, next) {
-  console.log(req.params.id)
-  res.send('you are in '+ req.params.id);
+  models.Reservation.findAll({where:{UserId: req.params.id}})
+  .then(function(reservations){
+    models.Spot.findAll({where:{UserId: req.params.id}})
+    .then(function(spots){
+      res.send({
+        reservations: reservations,
+        spots: spots
+      });
+    })
+  })
+});
+
+// User update route
+router.put('/:id', function(req, res, next){
+  res.send('you are updating user id ' + req.params.id)
 });
 
 // User delete route
 router.delete('/:id', function(req, res, next){
   res.send('you are deleting user id ' + req.params.id)
-})
+});
 
+// User login route
+router.get('/login', function(req, res, next) {
+  res.send('form for login');
+});
 
 // User login route
 router.post('/login', function(req, res, next) {
-
-  res.send('Run login process here');
+  res.send('submitting login form');
 });
 
 // User logout route

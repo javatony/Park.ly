f06aeac625e166471b99c8f782c3bfcb5684170e
Daniel Huang & Si Cheng Zhou <pair+danielkyh.szow79@devbootcamp.com>
2015-11-01 19:11:14 -0800

    add query feature, more testing coming

--- Park.ly/app.js
+++ Park.ly/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var cheese = require('./routes/cheese');
 var reservations = require('./routes/reservations');
 var spots = require('./routes/spots');
 var users = require('./routes/users');
@@ -74,6 +75,8 @@ app.use('/', routes);
 app.use('/reservations', reservations);
 app.use('/spots', spots);
 app.use('/users', users);
+app.use('/cheese',cheese);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

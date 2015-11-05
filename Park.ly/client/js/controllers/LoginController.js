43c24da8439946a2b4b6d3b13ec8c88f2618d978
Tony Tang <tang.tony7@gmail.com>
2015-11-05 12:47:46 -0800

    fix show page bug where it would display user welcome and added session check for index page pic

--- Park.ly/client/js/controllers/LoginController.js
+++ Park.ly/client/js/controllers/LoginController.js
@@ -3,10 +3,8 @@ app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModal
 
   $scope.processForm = function(){
     var data = angular.copy($scope.formData)
-    console.log(data)
     $http({
       method: 'POST',
-      // withCredentials: true,
       url: 'http://localhost:3001/users/login',
       data: data,
       headers: {
@@ -14,36 +12,15 @@ app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModal
       }
     })
     .success(function(response){
-      console.log("inside success repsonse")
       document.cookie = response;
-
       $uibModalInstance.dismiss('cancel')
       $window.location.reload()
     })
     .error(function(response){
       $scope.loginError = response;
-
       $scope.error = function(){
         return true
       }
-    })
-  }
-
-  if($routeParams.u_id){
-    $http({
-      method: 'GET',
-      url: ('http://localhost:3001/users/' + $routeParams.u_id),
-      headers:{
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    })
-    .success(function(response){
-      console.log(response)
-      $scope.reservations = response.reservations
-      $scope.spots = response.spots
-    })
-    .error(function(err){
-      console.log(err.error)
     })
   }
 
@@ -54,7 +31,5 @@ app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModal
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
-
-
 
 }]);

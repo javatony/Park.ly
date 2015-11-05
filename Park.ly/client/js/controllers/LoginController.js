b378e9c51e078eac2664fd46125a433f60bef548
Si Cheng Zhou <email": null>
2015-11-04 17:53:12 -0800

    pull request for comparing file changes

--- Park.ly/client/js/controllers/LoginController.js
+++ Park.ly/client/js/controllers/LoginController.js
@@ -1,4 +1,4 @@
-app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModalInstance', '$rootScope', function($scope, $http, $routeParams, $uibModalInstance, $rootScope){
+app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModalInstance', '$rootScope', '$window', function($scope, $http, $routeParams, $uibModalInstance, $rootScope, $window){
   $scope.formData = {}
 
   $scope.processForm = function(){
@@ -17,6 +17,7 @@ app.controller('LoginController', ['$scope', '$http', '$routeParams', '$uibModal
       console.log("inside success repsonse")
       document.cookie = response;
       $uibModalInstance.dismiss('cancel')
+      $window.location.reload()
     })
     .error(function(response){
       $scope.loginError = response
